9530c57aff671a346096ae1fbb7d489f7b1c4a84
srishti2007 <64143148+srishti2007@users.noreply.github.com>
2020-06-07 16:59:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var drawing = [];
+var new_drawing = [];
+var canvas;
+var database;
+
+function setup(){
+
+    canvas = createCanvas(displayWidth+20, displayHeight);
+
+    database = firebase.database();
+
+}
+
+function mouseDragged(){
+
+    var point={
+    x : mouseX,
+    y : mouseY
+  }
+  drawing.push(point);
+  var drawingRef = database.ref('drawing');
+  drawingRef.set({
+    "d":drawing
+  })
+} 
+
+function draw(){
+  background(0);
+
+  readData();
+
+  beginShape();
+
+  strokeWeight(4);
+
+  stroke("pink");
+
+  fill(0);
+  for(var i = 0; i< new_drawing.length; i++){
+    vertex(new_drawing[i].x, new_drawing[i].y);
+
+    endShape();
+
+  }
+}
+
+function readData(){
+  database.ref('drawing').on('value', (data) =>{
+    new_drawing = data.val().d;
+  })
+}
